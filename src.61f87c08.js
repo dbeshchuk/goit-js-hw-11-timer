823047e8eadfb42afac9550616e7544cd4408530
dbeshchuk <dmytro.beshchuk@protonmail.com>
2021-05-19 20:22:29 +0300

    Updates

--- src.61f87c08.js
+++ src.61f87c08.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10/src.61f87c08.js.map
+//# sourceMappingURL=/goit-js-hw-11-timer/src.61f87c08.js.map
